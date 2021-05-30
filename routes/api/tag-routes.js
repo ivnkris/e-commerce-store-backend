dd6d454ac125c25893196891ff26a3dc7ffb5b63
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-05-30 16:21:28 +0100

    resolved issue with MODULE_NOT_FOUND error

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { regexp } = require("sequelize/types/lib/operators");
+const { regexp } = require("sequelize");
 const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
