6989c544e75158043a8f67e92e5db628e95c9a18
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-05-30 15:10:54 +0100

    added get one tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,27 +1,64 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tags = await Tag.findAll({
+      include: [
+        {
+          model: Product,
+          attributes: ["product_name", "price", "stock"],
+        },
+      ],
+    });
+    res.json(tags);
+  } catch (err) {
+    console.log(`[ERROR] - ${err.message}`);
+    res.status(500).json({ error: "Failed to get tags" });
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tag = await Tag.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [
+        {
+          model: Product,
+          attributes: ["product_name", "price", "stock"],
+        },
+      ],
+    });
+    if (!tag) {
+      res.status(404).json({
+        error: "Tag does not exist",
+      });
+    } else {
+      res.json(tag);
+    }
+  } catch (err) {
+    console.log(`[ERROR] - ${err.message}`);
+    res.status(500).json({ error: "Failed to get tag" });
+  }
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
 });
 
