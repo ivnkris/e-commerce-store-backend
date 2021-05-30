9399f8d78997e4d1ee6c401fb943058e643208ef
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-05-30 15:46:19 +0100

    added database sync to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const routes = require("./routes");
 // import sequelize connection
+const connection = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,6 +13,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+const init = async () => {
+  try {
+    await connection.sync({ force: true });
+
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`);
+    });
+  } catch (err) {
+    console.log(`Connection to DB failed: ${err.message}`);
+  }
+};
+
+init();
